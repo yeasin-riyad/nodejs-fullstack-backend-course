0ea0856534b9ae3b14c476e8a953e1faa038a3c9
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-03-26 00:00:26 +0600

    async-await completed

---
+++ 1.Node-Tutorial/9.async-await/index.js
@@ -0,0 +1,27 @@
+async function delayFn(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function delayedGreet(name) {
+  await delayFn(2000);
+  console.log(name);
+}
+
+delayedGreet("Sangam");
+
+async function division(num1, num2) {
+  try {
+    if (num2 === 0) throw new Error("Can not divide by 0");
+    return num1 / num2;
+  } catch (error) {
+    console.error("error", error.message);
+    return null;
+  }
+}
+
+async function mainFn() {
+  console.log(await division(10, 2));
+  console.log(await division(10, 0));
+}
+
+mainFn();
