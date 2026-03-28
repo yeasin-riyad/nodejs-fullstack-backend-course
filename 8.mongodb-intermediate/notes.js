c898544243d8f71209b032929225e9c1e4b88950
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-03-28 22:43:21 +0600

    mongodb-intermediate project initialization

---
+++ 8.mongodb-intermediate/notes.js
@@ -0,0 +1,13 @@
+// What we will cover in this file:
+// 1.Understanding the aggrigation pipeline in MongoDB
+// 2.Using the $match stage in the aggrigation pipeline
+// 3.Using the $group stage in the aggrigation pipeline
+// 4.Using the $sort stage in the aggrigation pipeline
+// 5.Using the $project stage in the aggrigation pipeline
+// 6.Using the $lookup stage in the aggrigation pipeline
+// 7.Using the $unwind stage in the aggrigation pipeline
+// 8.Using the $limit stage in the aggrigation pipeline
+// 9.Using the $skip stage in the aggrigation pipeline
+//10.Using common aggrigation operators in the aggrigation pipeline
+//11.Understanding documents referencing and embedding in MongoDB
+//12.Populating the referenced documents
